cfbe4e77f9cd839f45e6f77906150e24fdc88122
Ruiwen Chua <ruiwen@thoughtmonkeys.com>
2018-05-28 14:43:37 +0800

    fix(elastic/cmd/root) set a proper name of the command to 'elastic'

--- cmd/root.go
+++ cmd/root.go
@@ -78,11 +78,8 @@ func initConfig() {
 }
 
 var rootCmd = &cobra.Command{
-	Use:   "hugo",
-	Short: "Hugo is a very fast static site generator",
-	Long: `A Fast and Flexible Static Site Generator built with
-                love by spf13 and friends in Go.
-                Complete documentation is available at http://hugo.spf13.com`,
+	Use:   "elastic",
+	Short: "elastic is a handy commandline Elasticsearch client meant for admins",
 	Run: func(cmd *cobra.Command, args []string) {
 		// Do Stuff Here
 	},
